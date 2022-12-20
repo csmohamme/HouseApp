930a79cf14e95fafb8086b25c75fbdefb86b311b
Mohammed Almostafa <58958280+csmohamme@users.noreply.github.com>
2022-12-20 00:10:56 -0800

    design & css in card_component

--- FrontEnd/src/app/property-list/property-list.component.ts
+++ FrontEnd/src/app/property-list/property-list.component.ts
@@ -6,7 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Name": "Mostafa",
+      "Type": "House"
+    },
+    {
+      "Id": 2,
+      "Name": "Mostafa",
+      "Type": "House"
+    },
+    {
+      "Id": 3,
+      "Name": "Mostafa",
+      "Type": "House"
+    },
+    {
+      "Id": 4,
+      "Name": "Mostafa",
+      "Type": "House"
+    },
+    {
+      "Id": 5,
+      "Name": "Mostafa",
+      "Type": "House"
+    },
+    {
+      "Id": 6,
+      "Name": "Mostafa",
+      "Type": "House"
+    },
+    {
+      "Id": 7,
+      "Name": "Mostafa",
+      "Type": "House"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
